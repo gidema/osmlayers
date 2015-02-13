76e2ac322cac18819ccc0c2fce18a90ac42ffe90
gidema <mail@gertjanidema.nl>
2015-02-14 00:17:25 +0100

    Added widgets and popup with tabs.

--- js_source/FeaturePopup.js
+++ js_source/FeaturePopup.js
@@ -2,8 +2,7 @@
  * FeaturePopup The FeaturePopup class creates the Popup window for the info
  * about the selected OSM feature
  */
-FeaturePopup = OpenLayers
-        .Class({
+FeaturePopup = OpenLayers.Class({
             genericUrl : null,
             map : null,
             popup : null,
@@ -42,7 +41,10 @@ FeaturePopup = OpenLayers
                     type : fid[0],
                     id : fid[1],
                     tags : feature.attributes,
+                    usedTags : {},
                     lonlat : lonlat,
+                    lon : lonlat.lon,
+                    lat : lonlat.lat,
                     zoom : zoom
                 });
             },
@@ -71,212 +73,63 @@ FeaturePopup = OpenLayers
             },
 
             addMainTab : function(data) {
-                var html = '<div class="poi">\n';
-                var name = data.tags.name;
-                if (name) {
-                    html += '<h3>' + name + '</h3>\n';
-                }
-                var address = this.processAddress(data.tags);
-                if (address)
-                    html += address;
-                // process the rest of the tags
-                var self = this;
-                $.each(data.tags, function(key, val) {
-                    var tagHtml = self.processTag(key, val, address);
-                    if (tagHtml) {
-                        html += key + ": " + tagHtml + "<br />\n";
-                    }
-                });
-                html += '</div>';
+                var widgets = [
+                    new osml.widgets.Title(data),
+                    new osml.widgets.Address(data),
+                    new osml.widgets.Phone(data),
+                    new osml.widgets.Email(data),
+                    new osml.widgets.Website(data, 'www'),
+                    new osml.widgets.Twitter(data, 'url'),
+                    new osml.widgets.Facebook(data),
+                    new osml.widgets.Wikipedia(data)
+                ];
+                var widget = new osml.widgets.WidgetGroup(data, widgets, 'plain');
+                widget.check();
+                var html = widget.toHtml();
                 this.addTab('popup-main', 'General', html);
             },
 
             addDetailTab : function(data) {
-                // process the open streetmap link
-                html = '<strong><a target = "_blank" href="http://www.openstreetmap.org/browse/'
-                        + data.type
-                        + "/"
-                        + data.id
-                        + '">'
-                        + data.type
-                        + " "
-                        + data.id + "</a></strong><br ?>\n";
+                var widgets = [
+                    new osml.widgets.BrowseOsm(data),
+                    new osml.widgets.UnusedTags(data)
+                ];
+                var widget = new osml.widgets.WidgetGroup(data, widgets, 'plain');
+                widget.check();
+                var html = widget.toHtml();
                 this.addTab('popup-detail', 'Detail', html);
             },
             
             addViewTab : function(data) {
-                var lon = data.lonlat.lon;
-                var lat = data.lonlat.lat;
-                var zoom = data.zoom;
-                var html = '<ul>' +
-                    '<li>' + this.viewOsm(lon, lat, zoom) + '</li>\n' +
-                    '<li>' + this.viewGoogle(lon, lat, zoom) + '</li>\n' +
-                    '<li>' + this.viewBing(lon, lat, zoom) + '</li>\n' +
-                    '<li>' + this.viewMtM(lon, lat, zoom) + '</li>\n' +
-                    '<li>' + this.viewMapillary(lon, lat, zoom) + '</li>\n' +
-                  '</ul>';
+                var widgets = [
+                    new osml.widgets.ViewOsm(data),
+                    new osml.widgets.ViewGoogle(data),
+                    new osml.widgets.ViewBing(data),
+                    new osml.widgets.ViewMtM(data),
+                    new osml.widgets.ViewMapillary(data),
+                    new osml.widgets.ViewBagViewer(data)
+                ];
+                var widget = new osml.widgets.WidgetGroup(data, widgets, 'ul');
+                var html = '';
+                if (widget.check()) {
+                    html += widget.toHtml();
+                }
                 this.addTab('popup-view', 'View', html);
             },
             addEditTab : function(data) {
-                // Hoe wordt de te bewerken oppervlakte berekend voor JOSM?
-                // var area = 0.01 // oorspronkelijke waarde
-                // mz: Gegevensset kleiner gemaakt voor josm
-
-                var lon = data.lonlat.lon;
-                var lat = data.lonlat.lat;
-                var zoom = data.zoom;
-                var area = 0.005; // was 0.01
-                var ctop = lat + area;
-                var cbottom = ctop - (2 * area);
-                var cleft = lon - area;
-                var cright = cleft + (2 * area);
-
-                var html = '<b>Edit area with:</b><br><a href="http://localhost:8111/load_and_zoom?top='
-                        + ctop
-                        + '&bottom='
-                        + cbottom
-                        + '&left='
-                        + cleft
-                        + '&right='
-                        + cright
-                        + '" target="josm_frame">JOSM</a>&nbsp&nbsp';
-                html += '<a href="http://www.openstreetmap.org/edit?editor=id&lat='
-                        + lat + '&lon=' + lon + '&zoom=' + zoom
-                        + '" target="_blank">ID&nbsp;editor</a>&nbsp&nbsp';
-                html += '<a href="http://www.openstreetmap.org/edit?editor=potlatch2&lat='
-                        + lat + '&lon=' + lon + '&zoom=' + zoom
-                        + '" target="_blank">Potlatch&nbsp;2</a>&nbsp&nbsp';
-                html += '</div>'; 
+                var widgets = [
+                    new osml.widgets.EditJosm(data),
+                    new osml.widgets.EditOnline(data, 'id'),
+                    new osml.widgets.EditOnline(data, 'potlatch')
+                ];
+                var html = '<h2>Edit area with:</h2>';
+                html += '<ul>';
+                for (var i = 0; i<widgets.length; i++) {
+                    var widget = widgets[i];
+                    html += '<li>' + widget.toHtml() + '</li>\n';
+                };
+                html += '</ul>';
                 this.addTab('popup-edit', 'Edit', html);
-            },
-
-            viewOsm : function(lon, lat, zoom) {
-                var url = String.format('http://www.openstreetmap.org?lat={0}&lon={1}&zoom={2}', lat, lon, zoom);
-                return String.format('<a href="{0}" target="_blank\"><img src="img/osm.gif">OSM</a>', url);
-            },
-            viewGoogle : function(lon, lat, zoom) {
-                var url = String.format('https://maps.google.nl/maps?ll={0},{1}&t=h&z={2}', lat, lon, zoom);
-                return String.format('<a href="{0}" target="_blank"><img src="img/google.gif">Google</a>', url);
-            },
-            viewBing : function(lon, lat, zoom) {
-                var url = String.format('http://www.bing.com/maps/?v=2&cp={0}~{1}&lvl={2}&dir=0&sty=h&form=LMLTCC', 
-                     lat, lon, zoom);
-                return String.format('<a href="{0}" target="_blank"><img src="img/bing.gif">Bing</a>', url);
-            },
-            viewMtM : function(lon, lat, zoom) {
-                var url = String.format('http://mijndev.openstreetmap.nl/~allroads/mtm/?map=roads' +
-                        '&zoom={0}&lat={1}&lon={2}&layers=B000000FFFFFFFFFFFFTFF', zoom, lat, lon);
-                return String.format('<a href="{0}" target="_blank\"><img src="img/osm.gif">MtM</a>', url);
-            },
-            viewMapillary : function(lon, lat, zoom) {
-                var url = String.format('http://www.mapillary.com/map/im/bbox/' +
-                    '{0}/{1}/{2}/{3}', (lat - 0.005), (lat + 0.005), (lon - 0.005), (lon + 0.005));
-                return String.format('<a href="{0}" target="_blank\"><img src="img/mapillary.png">Mapillary</a>', url);
-            },
-            /*
-             * Create the html for a single tag
-             */
-            processTag : function(key, value, address) {
-                var k = key.split(":");
-                switch (k[0]) {
-                case "website":
-                case "twitter":
-                case "facebook":
-                case "email":
-                case "phone":
-                case "fax":
-                case "url":
-                    return this.processContactTag(k[0], value);
-                case "contact":
-                    if (k.length > 1) {
-                        return this.processContactTag(k[1], value);
-                    }
-                    return key;
-                case "wikipedia":
-                    if (k.length == 2) {
-                        var lang = k[1] + ".";
-                    } else {
-                        lang = "";
-                    }
-                    var s = value.split(':'); // Subject
-                    if (s.length == 2) {
-                        lang = s[0] + ".";
-                        subject = s[1];
-                    } else {
-                        subject = value;
-                    }
-                    var href = "http://" + lang + "wikipedia.org/wiki/"
-                            + subject;
-                    return this.makeLink(href, value, true);
-                case "addr":
-                    return (address ? null : value);
-                case "name":
-                    // Ignore the name tag because we show it in the header
-                    return null;
-                default:
-                    return value;
-                }
-            },
-
-            processContactTag : function(key, value) {
-                switch (key) {
-                case "website":
-                case "url":
-                    return this.makeLink(value, value, true);
-                case "twitter":
-                    return this.makeLink("https://twitter.com/" + value, value,
-                            true);
-                case "facebook":
-                    if (value.startsWith("http") || value.startsWith("www")
-                            || value.startsWith("facebook")) {
-                        return this.makeLink(value, value, true);
-                    }
-                    return this.makeLink("https://www.facebook.com/" + value,
-                            value, true);
-                case "email":
-                    return this.makeLink("mailto:" + value, value, true);
-                case "phone":
-                    return this.makeLink("tel:" + value, value, true);
-                case "fax":
-                    return this.makeLink("fax:" + value, value, true);
-                default:
-                    return value;
-                }
-            },
-
-            /*
-             * Create the html for a nicely formatted address.
-             */
-            processAddress : function(tags) {
-                var street = tags["addr:street"];
-                var number = tags["addr:housenumber"];
-                if (!(street && number))
-                    return null;
-                var postcode = tags["addr:postcode"];
-                var city = tags["addr:city"];
-                var html = street + " " + number + "<br />\n";
-                if (postcode)
-                    html += postcode;
-                if (postcode && city)
-                    html += "  ";
-                if (city)
-                    html += city;
-                if (postcode || city)
-                    html += "<br />\n";
-                return html;
-            },
-
-            /*
-             * Create the html for a link
-             */
-            makeLink : function(href, text, newPage) {
-                var html = "<a ";
-                if (newPage)
-                    html += 'target="_blank" ';
-                if (href.indexOf(":") == -1) {
-                    return html + 'href=//"' + href + '">' + text + "</a>";
-                }
-                return html + 'href="' + href + '">' + text + "</a>";
             },
             CLASS_NAME : "FeaturePopup"
         });
@@ -284,9 +137,10 @@ FeaturePopup = OpenLayers
 String.format = function() {
     var s = arguments[0];
     for (var i = 0; i < arguments.length - 1; i++) {       
-      var reg = new RegExp("\\{" + i + "\\}", "gm");             
+      var reg = new RegExp('\\{" + i + "\\}', 'gm');             
       s = s.replace(reg, arguments[i + 1]);
     }
 
     return s;
 };
+
---
+++ js_source/widgets.js
@@ -0,0 +1,438 @@
+// Temporarily define the osml namespace here.
+if (typeof osml === 'undefined') osml = {};
+if (typeof osml.widgets === 'undefined') osml.widgets = {};
+
+osml.widgets.Title = function(data) {
+    var title = '';
+    this.check = function() {
+        var tags = data.tags;
+        title = tags.name;
+        if (!title) return false;
+        data.usedTags.name = true;
+        return true;
+    };
+    this.toHtml = function() {
+        return '<h2 class="title">' + title + '</h2>\n';
+    };
+};
+
+osml.widgets.Address = function(data) {
+    this.street = '';
+    this.number = '';
+    this.postcode = '';
+    this.city = '';
+    this.check = function() {
+        var tags = data.tags;
+        this.street = tags['addr:street'];
+        this.number = tags['addr:housenumber'];
+        if (!this.street || !this.number) {
+            return false;
+        };
+        this.postcode = tags['addr:postcode'];
+        this.city = tags['addr:city'];
+        data.usedTags['addr:street'] = true;
+        data.usedTags['addr:housenumber'] = true;
+        data.usedTags['addr:postcode'] = true;
+        data.usedTags['addr:city'] = true;
+        return true;
+    };
+    
+    this.toHtml = function() {
+        var html = this.street + '&nbsp;' + this.number + '<br />\n' +
+            (this.postcode ? this.postcode + '&nbsp;&nbsp;' : '') +
+            (this.city ? this.city : '') +
+            (this.postcode || this.city ? '<br />\n' : '');
+        return '<div class="address">' + html + '</div>';
+    };
+};
+
+osml.widgets.Website = function(data, type) {
+    var site = '';
+    this.check = function() {
+        site = data.tags.website;
+        if (!site) return false;
+        data.usedTags.website = true;
+        return true;
+    };
+    this.toHtml = function() {
+        var link = osml.widgets.makeLink(site, site, true);
+        return '<div class="website">' + link + '</div>';
+    };
+};
+
+osml.widgets.Phone = function(data) {
+    var phone = '';
+    this.check = function() {
+        phone = data.tags.phone;
+        if (!phone) return false;
+        data.usedTags.phone = true;
+        return true;
+    };
+    this.toHtml = function() {
+        var link = osml.widgets.makeLink("tel:" + phone, phone, true);
+        return '<div class="phone">' + link + '</div>';
+
+    };
+};
+
+osml.widgets.Email = function(data) {
+    var email = data.tags.email;
+    this.check = function() {
+        if (!email) return false;
+        data.usedTags.email = true;
+        return true;
+    };
+    this.toHtml = function() {
+        var link = osml.widgets.makeLink("mailto:" + email, email, true);
+        return '<div class="email">' + link + '</div>';
+    };
+};
+osml.widgets.Fax = function(data) {
+    var fax = data.tags.fax;
+    this.check = function() {
+        if (!fax) return false;
+        data.usedTags.fax = true;
+        return true;
+    };
+    this.toHtml = function() {
+        var link = osml.widgets.makeLink("fax:" + fax, fax, true);
+        return '<div class="fax">Fax:&nbsp;' + link + '</div>';
+    };
+};
+osml.widgets.Twitter = function(data) {
+    var twitter = data.tags.twitter;
+    this.check = function() {
+        if (!twitter) return false;
+        data.usedTags.twitter = true;
+        return true;
+    };
+    this.toHtml = function() {
+        var link = osml.widgets.makeLink('https://twitter.com/' + twitter, '@' + twitter, true);
+        return '<div class="twitter">Twitter:&nbsp;' + link + '</div>';
+    };
+};
+
+osml.widgets.Facebook = function(data) {
+    var value = data.tags.facebook;
+    this.check = function() {
+        if (!value) return false;
+        data.usedTags.facebook = true;
+        return true;
+    };
+    this.toHtml = function() {
+        var link = '';
+        if (value.startsWith('http') || value.startsWith('www')
+                || value.startsWith('facebook')) {
+            link = osml.widgets.makeLink(value, value, true);
+        }
+        else {
+            link = osml.widgets.makeLink('https://www.facebook.com/' + value, value, true);
+        };
+        return '<div class="facebook">Facebook:&nbsp;' + link + '</div>';
+    };
+};
+
+osml.widgets.Wikipedia = function(data) {
+    this.wiki = {};
+    this.lang = '';
+    
+    this.check = function() {
+        var tags = data.tags;
+        for (var key in tags) {
+            if (key.substr(0, 9) == 'wikipedia') {
+                this.wiki[key] = tags[key];
+                data.usedTags[key] = true;
+            };
+        };
+        if (this.wiki.length == 0) return false;
+        return true;
+    };
+    
+    this.toHtml = function() {
+        var html = '';
+        for (var key in this.wiki) {
+            html = this.wikiToHtml(key);
+        }
+        return html;
+    };
+    
+    this.wikiToHtml = function(key) {
+        k = key.split(':');
+        if (k.length == 2) {
+            this.lang = k[1] + '.';
+        }
+        var value = this.wiki[key];
+        var s = value.split(':'); // Subject
+        if (s.length == 2) {
+            this.lang = s[0] + '.';
+            this.subject = s[1];
+        } else {
+            this.subject = value;
+        }
+        var href = 'http://' + this.lang + 'wikipedia.org/wiki/'
+                + this.subject;
+        return '<div class="wikipedia">' + osml.widgets.makeLink(href, 'Wikipedia', true) + '</div>';
+    };
+};
+
+osml.widgets.BrowseOsm = function(data) {
+    this.check = function() {
+        return true;
+    };
+    
+    this.toHtml = function() {
+        var url = osml.widgets.formatString('http://www.openstreetmap.org/browse/{0}/{1}/', data.type, data.id);
+        var label = data.type + " " + data.id;
+        return osml.widgets.formatString('<a href="{0}" target="_blank\">{1}</a>', url, label);
+    };
+};
+
+osml.widgets.UnusedTags = function(data) {
+    this.check = function() {
+        return true;
+    };
+    
+    this.toHtml = function() {
+        var html = '<dl>';
+        $.each(data.tags, function(key, val) {
+            if (!data.usedTags[key]) {
+                var url = osml.widgets.formatString("http://wiki.openstreetmap.org/wiki/Key:{0}", key);
+                html += osml.widgets.formatString('<dt><a href="{0}" target="_blank">{1}</a></dt><dd>{2}</dd>\n', url, key, val);
+            }
+        });
+        html += '</dl>';
+        return html;
+    };
+};
+
+/**
+ * 
+ * @param data
+ * @returns {osml.widgets.ViewOsm}
+ */
+osml.widgets.ViewOsm = function(data) {
+    var params = {
+        lat : data.lat,
+        lon : data.lon,
+        zoom : data.zoom
+    };
+    
+    this.check = function() {
+        return true;
+    };
+    
+    this.toHtml = function() {
+        var url = osml.widgets.formatUrl('http://www.openstreetmap.org', params);
+        return osml.widgets.formatString('<a href="{0}" target="_blank\"><img src="img/osm.gif">OSM</a>', url);
+    };
+};
+
+osml.widgets.ViewGoogle = function(data) {
+    var params = {
+        ll : data.lat + ',' + data.lon,
+        zoom : data.zoom,
+        t : 'h'
+    };
+    
+    this.check = function() {
+        return true;
+    };
+    
+    this.toHtml = function() {
+        var url = osml.widgets.formatUrl('https://maps.google.nl/maps', params);
+        return osml.widgets.formatString('<a href="{0}" target="_blank"><img src="img/google.gif">Google</a>', url);
+    };
+};
+
+osml.widgets.ViewBing = function(data) {
+    var params = {
+        v : '2',
+        cp : data.lat + '~' + data.lon,
+        lvl : data.zoom,
+        dir : '0',
+        sty : 'h',
+        form : 'LMLTCC'
+    };
+    this.check = function() {
+        return true;
+    };
+    
+    this.toHtml = function() {
+        var url = osml.widgets.formatUrl('http://www.bing.com/maps/', params);
+        return osml.widgets.formatString('<a href="{0}" target="_blank"><img src="img/bing.gif">Bing</a>', url);
+    };
+};
+
+osml.widgets.ViewMtM = function(data) {
+    var params = {
+        map : 'roads',
+        zoom : data.zoom,
+        lat : data.lat,
+        lon : data.lon,
+        layers : 'B000000FFFFFFFFFFFFTFF'
+    };
+    
+    this.check = function() {
+        return true;
+    };
+    
+    this.toHtml = function() {
+        var url = osml.widgets.formatUrl('http://mijndev.openstreetmap.nl/~allroads/mtm/', params);
+        return osml.widgets.formatString('<a href="{0}" target="_blank\"><img src="img/osm.gif">MtM</a>', url);
+    };
+};
+osml.widgets.ViewMapillary = function(data) {
+    var lat = data.lat;
+    var lon = data.lon;
+    
+    this.check = function() {
+        return true;
+    };
+    
+    this.toHtml = function() {
+        var url = osml.widgets.formatString('http://www.mapillary.com/map/im/bbox/{0}/{1}/{2}/{3}',
+            (lat - 0.005), (lat + 0.005), (lon - 0.005), (lon + 0.005));
+        return osml.widgets.formatString('<a href="{0}" target="_blank\"><img src="img/mapillary.png">Mapillary</a>', url);
+    };
+};
+
+osml.widgets.ViewBagViewer = function(data) {
+    var bagId = data.tags['ref:bag'];
+    
+    this.check = function() {
+        if (bagId) {
+            data.usedTags['ref:bag'] = true;
+            return true;
+        }
+    };
+    
+    this.toHtml = function() {
+        var id = OpenLayers.Number.zeroPad(bagId, 16);
+        var params = {
+            searchQuery : id
+        };
+        var url = osml.widgets.formatUrl('https://bagviewer.kadaster.nl/lvbag/bag-viewer/index.html#/', params);
+        return osml.widgets.formatString('<a href="{0}" target="_blank\">BAG Viewer: {1}</a>', url, id);
+    };
+};
+
+osml.widgets.EditJosm = function(data) {
+    var area = 0.002; // was 0.01
+    var top = data.lat + area;
+    var left = data.lon - area;
+    var params = {
+        top : top,
+        bottom : top - (2 * area),
+        left : left,
+        right : left + (2 * area),
+        select : data.type + data.id
+    };
+    
+    this.check = function() {
+        return true;
+    };
+
+    this.toHtml = function() {
+        var url = osml.widgets.formatUrl('http://localhost:8111/load_and_zoom', params);
+        return osml.widgets.formatString('<a href="{0}" target="_blank">JOSM</a>', url);
+
+    };
+};
+
+osml.widgets.EditOnline = function(data, type) {
+    var params = {
+            editor : type,
+            lon : data.lon,
+            lat : data.lat,
+            zoom : data.zoom
+    };
+    
+    this.check = function() {
+        return true;
+    };
+    
+    this.toHtml = function() {
+        var name = (type == 'id' ? 'ID&nbsp;editor' : 'Potlatch&nbsp2');
+        var url = osml.widgets.formatUrl('http://www.openstreetmap.org/edit', params);
+        return osml.widgets.formatString('<a href="{0}" target="_blank">{1}</a>', url, name);
+    };
+};
+
+osml.widgets.WidgetGroup = function(data, widgets, format) {
+    this.check = function() {
+        var check = false;
+        for (var i = 0; i<widgets.length; i++) {
+            check = check || widgets[i].check();
+        }
+        return check;
+    };
+    
+    this.toHtml = function() {
+        switch (format) {
+        case 'plain':
+            return this.toHtmlPlain();
+        case 'ul':
+            return this.toHtmlUl();
+        }
+    };
+    
+    this.toHtmlPlain = function() {
+        var html = '';
+        for (var i = 0; i<widgets.length; i++) {
+            var widget = widgets[i];
+            if (widget.check()) {
+                html += widget.toHtml() + '\n';
+            };
+        };
+        return html;
+    };
+    
+    this.toHtmlUl = function() {
+        var html = '<ul>';
+        for (var i = 0; i<widgets.length; i++) {
+            var widget = widgets[i];
+            if (widget.check()) {
+                html += '<li>' + widget.toHtml() + '</li>\n';
+            };
+        };
+        html += '</ul>';
+        return html;
+    };
+};
+
+osml.widgets.makeLink = function(href, text, newPage) {
+    var html = "<a ";
+    if (newPage)
+        html += 'target="_blank" ';
+    if (href.indexOf(":") == -1) {
+        return html + 'href=//"' + href + '">' + text + "</a>";
+    }
+    return html + 'href="' + href + '">' + text + "</a>";
+};
+
+osml.widgets.formatString = function() {
+    var s = arguments[0];
+    for (var i = 0; i < arguments.length - 1; i++) {       
+      var reg = new RegExp('\\{' + i + '\\}', 'gm');             
+      s = s.replace(reg, arguments[i + 1]);
+    }
+    return s;
+};
+
+osml.widgets.formatUrl = function(url, params) {
+    var u = url;
+    var first = true;
+    for (var key in params) {
+        if (first === true) {
+            u = u + '?';
+            first = false;
+        }
+        else {
+            u = u + '&';
+        }
+        u = u + key + '=' + params[key];
+    }
+    return u;
+};
+
+
